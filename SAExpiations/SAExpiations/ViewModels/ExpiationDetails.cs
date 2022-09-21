d0b234692b5941a5e0c50461445b920d0b21e158
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-09-21 12:50:07 +0930

    able to get values from the database for that specific expiation

--- SAExpiations/SAExpiations/ViewModels/ExpiationDetails.cs
+++ SAExpiations/SAExpiations/ViewModels/ExpiationDetails.cs
@@ -2,9 +2,10 @@
 {
     public class ExpiationDetails
     {
+
         public string ExpiationOffenceCode { get; set; } = null!;
         public string NoticeStatusDesc { get; set; } = null!;
-        public DateTime IssueDate { get; set; }
+        public string IssueDate { get; set; }
         public int StatusCount { get; set; }
 
 
