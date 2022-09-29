b94342649e2659738028c91c092fca7854e03898
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-09-29 11:14:31 +0930

    removed extra spaces

--- SAExpiations/SAExpiations/ViewModels/ExpiationDetails.cs
+++ SAExpiations/SAExpiations/ViewModels/ExpiationDetails.cs
@@ -2,13 +2,9 @@
 {
     public class ExpiationDetails
     {
-
         public string ExpiationOffenceCode { get; set; } = null!;
         public string NoticeStatusDesc { get; set; } = null!;
         public int IssueDate { get; set; }
         public int StatusCount { get; set; }
-
-
-
     }
 }
